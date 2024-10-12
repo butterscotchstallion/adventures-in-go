855f30944d78fb3ed1f907a6bd2d58619a132219
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-10-12 14:59:49 -0400

    Initial commit

---
+++ greetings/greetings.go
@@ -0,0 +1,44 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Howdy %v! Welcome!",
+		"Meowdy %v.",
+		"Hola %v! Que onda?",
+		"Bonjour %v",
+		"Hi there, %v",
+		"Hail, %v. Well met.",
+	}
+	return formats[rand.Intn(len(formats))]
+}
