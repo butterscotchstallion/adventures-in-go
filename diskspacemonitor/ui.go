4e8c832b863fcfcba0ebcfdff6be68cb8590e6b0
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-10-18 00:33:49 -0400

    Refactors to use Fyne for UI

--- diskspacemonitor/ui.go
+++ diskspacemonitor/ui.go
@@ -1,77 +1,27 @@
 package main
 
 import (
-	"os"
-
-	"gioui.org/app"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"go.uber.org/zap"
 )
 
-type C = layout.Context
-type D = layout.Dimensions
-
 func InitUI(logger *zap.SugaredLogger) {
 	logger.Debug("Initializing UI")
-	go func() {
-		window := new(app.Window)
-		window.Option(app.Title(appNameWithVersion))
-		window.Option(app.Size(unit.Dp(600), unit.Dp(400)))
-		// err := run(window, logger)
-		err := draw(window)
-		if err != nil {
-			logger.Fatal(err)
-		}
 
-		os.Exit(0)
-	}()
-	app.Main()
-}
+	a := app.New()
+	w := a.NewWindow(appNameWithVersion)
+	w.Resize(fyne.NewSize(600, 400))
 
-func draw(w *app.Window) error {
-	var ops op.Ops
-	var saveButton widget.Clickable
-	th := material.NewTheme()
-	for {
-		switch e := w.Event().(type) {
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-			layout.Flex{
-				Axis:    layout.Vertical,
-				Spacing: layout.SpaceStart,
-			}.Layout(gtx,
-				layout.Rigid(
-					func(gtx C) D {
-						// ONE: First define margins around the button using layout.Inset ...
-						margins := layout.Inset{
-							Top:    unit.Dp(15),
-							Bottom: unit.Dp(0),
-							Right:  unit.Dp(15),
-							Left:   unit.Dp(15),
-						}
-						// TWO: ... then we lay out those margins ...
-						return margins.Layout(gtx,
-							// THREE: ... and finally within the margins, we define and lay out the button
-							func(gtx C) D {
-								btn := material.Button(th, &saveButton, "Save Settings")
-								return btn.Layout(gtx)
-							},
-						)
-					},
-				),
-				layout.Rigid(
-					layout.Spacer{Height: unit.Dp(25)}.Layout,
-				),
-			)
-			e.Frame(gtx.Ops)
+	hello := widget.NewLabel("Hello world!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
-		// this is sent when the application is closed
-		case app.DestroyEvent:
-			return e.Err
-		}
-	}
+	w.ShowAndRun()
 }
