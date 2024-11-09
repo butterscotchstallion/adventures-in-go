38187ff5c535c4853123d4947fcbb3ac480ae8d2
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-08 23:11:31 -0500

    Adds more test cases

--- leetcode/valid_parenthesis.go
+++ leetcode/valid_parenthesis.go
@@ -1,5 +1,59 @@
 package leetcode
 
+/**
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ */
 func isValid(s string) bool {
-	return false
+	openParenCount := 0
+	closeParenCount := 0
+	openBracketCount := 0
+	closeBracketCount := 0
+	openBraceCount := 0
+	closeBraceCount := 0
+
+	if len(s) == 1 {
+		return false
+	}
+
+	for pos, char := range s {
+		if char == '(' {
+			openParenCount++
+		}
+		if char == ')' {
+			if openParenCount == closeParenCount {
+				return false
+			}
+			closeParenCount++
+		}
+		if char == '[' {
+			openBracketCount++
+			if pos < len(s)-1 && s[pos+1] != ']' {
+				return false
+			}
+		}
+		if char == ']' {
+			if openBracketCount == closeBracketCount {
+				return false
+			}
+			closeBracketCount++
+		}
+		if char == '{' {
+			openBraceCount++
+			if pos < len(s)-1 && s[pos+1] != '}' {
+				return false
+			}
+		}
+		if char == '}' {
+			if openBraceCount == closeBraceCount {
+				return false
+			}
+			closeBraceCount++
+		}
+	}
+	return openParenCount == closeParenCount && openBraceCount == closeBraceCount
 }
--- leetcode/valid_parenthesis_test.go
+++ leetcode/valid_parenthesis_test.go
@@ -3,22 +3,43 @@ package leetcode
 import "testing"
 
 func Test_isValid(t *testing.T) {
-	type args struct {
-		s string
+	validResult := isValid("()")
+	if !validResult {
+		t.Errorf("() expected to be valid")
 	}
-	type testCase struct {
-		name string
-		args args
-		want bool
-	}
-	var testCases = make([]testCase, 3)
-	testCases = append(testCases, testCase{name: "case one", args: args{s: "()"}, want: true})
 
-	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := isValid(tt.args.s); got != tt.want {
-				t.Errorf("isValid() = %v, want %v", got, tt.want)
-			}
-		})
+	validResult2 := isValid("()[]{}")
+	if !validResult2 {
+		t.Errorf("()[]{} expected to be valid")
+	}
+
+	validResult3 := isValid("([])")
+	if !validResult3 {
+		t.Errorf("([]) expected to be valid")
+	}
+
+	validResult4 := isValid("[")
+	if validResult4 {
+		t.Errorf("[ expected to be invalid")
+	}
+
+	validResult5 := isValid("{[]}")
+	if !validResult5 {
+		t.Errorf("{[]} expected to be valid")
+	}
+
+	invalidResult := isValid("(]")
+	if invalidResult {
+		t.Errorf("(] is expected to be invalid")
+	}
+
+	invalidResult2 := isValid("([)]")
+	if invalidResult2 {
+		t.Errorf("([)] is expected to be invalid")
+	}
+
+	invalidResult3 := isValid("(){}}{")
+	if invalidResult3 {
+		t.Errorf("(){}}{ expected to be invalid")
 	}
 }
